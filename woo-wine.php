9cbacb24419aea304b048732be26b84b37e13a6b
Christopher Bishop <domingobishop@gmail.com>
2018-07-07 21:25:01 +0100

    first commit

---
+++ woo-wine.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Woo wine
+ * Plugin URI: https://github.com/domingobishop/woo-wine
+ * Description: A Woocommerce plugin for wine.
+ * Version: 0.1 beta
+ * Author: Chris Bishop
+ * Author URI: https://github.com/domingobishop
+ * License: GPL2
+ */
+
+// Included functions
+include 'functions-global.php';
+
